a4fa3e78d8a6ba1cf236641d290798441b82abe9
RayaneKettana <rkettana@gmail.com>
2021-12-15 10:55:16 +0100

    add calculator page

---
+++ lib/calculator.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  State<Calculator> createState() => _Calculator();
+}
+
+class _Calculator extends State<Calculator> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
